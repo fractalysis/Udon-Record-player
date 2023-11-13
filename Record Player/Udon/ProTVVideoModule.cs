3a2e41c6556a5ed7a2362565c3a4d24a98581f4c
Fraccy <fractalysisofficial@gmail.com>
2023-11-13 03:12:24 -0700

    Add ProTV version

---
+++ Record Player/Udon/ProTVVideoModule.cs
@@ -0,0 +1,46 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using ArchiTech;
+
+namespace RecordPlayer
+{
+    public class ProTVVideoModule : UdonSharpBehaviour
+    {
+        public TVManagerV2 tv;
+        [HideInInspector] public VRCUrl url = VRCUrl.Empty;
+        [HideInInspector] public bool canControlVideoPlayer;
+        [HideInInspector] public bool isPlaying;
+        [HideInInspector] public float length;
+        [HideInInspector] public float currentTime;
+        [HideInInspector] public VRCUrl currentURL = VRCUrl.Empty;
+
+        public void _CheckURL()
+        {
+            currentURL = tv.url;
+        }
+
+        public void _UpdateVideoPlayerStatus()
+        {
+            isPlaying = tv.currentState == 1; // PLAYING
+            length = tv.videoDuration;
+            currentTime = tv.currentTime;
+        }
+
+        public void _UpdateCanControlVideoPlayer()
+        {
+            canControlVideoPlayer = tv._IsPrivilegedUser();
+        }
+
+        void Start()
+        {
+            tv._RegisterUdonSharpEventReceiver(this);
+        }
+
+        public void _PlayVideoViaModule()
+        {
+            tv._ChangeMediaTo(url);
+        }
+    }
+}
